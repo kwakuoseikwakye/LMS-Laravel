711a94bb5fd41d94fbe226895a72d78510125e24
Nana Osei Kwakye <nana@Nanas-MacBook-Air.local>
2024-11-18 09:59:59 +0900

    return api version

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,12 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BookController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/v1', function () {
+      return response()->json([
+          'app_name' => 'Library Management System',
+          'api_version' => '1.0.0',
+      ]);
+  });
 Route::group(['prefix' => 'v1'], function () {
       Route::post("login", [AuthController::class, "login"]);
       Route::post("signup", [AuthController::class, "signup"]);
